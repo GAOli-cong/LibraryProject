9efb5e11ad5bd9a1a3611ca9d4acae1ef7b8dfcd
gaolicong <1962946811@qq.com>
2022-08-30 22:39:35 +0800

    first commit

---
+++ app/src/main/java/com/glc/libraryproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.glc.libraryproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ glcCode/src/main/java/com/glc/glccode/GlcCodeUtils.java
@@ -0,0 +1,17 @@
+package com.glc.glccode;
+
+/**
+ * author：glc on 2022/8/30 22:14
+ * desc：代码工具类
+ */
+public class GlcCodeUtils {
+    /**
+     *  int求和
+     * @param x
+     * @param y
+     * @return
+     */
+    public static int add(int x,int y){
+        return x+y;
+    }
+}
