ecfcb9a151c1b884a5549286e18ce724b2d048b0
gaolicong <1962946811@qq.com>
2022-08-30 23:25:08 +0800

    use

--- app/src/main/java/com/glc/libraryproject/MainActivity.kt
+++ app/src/main/java/com/glc/libraryproject/MainActivity.kt
@@ -2,10 +2,18 @@ package com.glc.libraryproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.glc.glccode.GlcCodeUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //使用jitpack 引入的依赖中的求和方法
+       val num=GlcCodeUtils.add(1,1)
+        Log.d("MainActivity", "1加1求和：$num")
+        Toast.makeText(application, "1加1求和：$num", Toast.LENGTH_SHORT).show()
     }
 }
